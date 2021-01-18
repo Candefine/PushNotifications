2ca469a3b793784d41976c0c5a457e0c1960795f
Mariana Secchi Sanches <mariana.sanches@bmggranito.com.br>
2021-01-18 17:35:17 -0300

    agora vai

---
+++ lib/providers/PushProvider.dart
@@ -0,0 +1,32 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushProvider {
+  final FirebaseMessaging _fcm = FirebaseMessaging();
+
+  initNotfy() {
+    _fcm.requestNotificationPermissions(); 
+    //Token Get
+    _fcm.getToken().then((token){
+        print('FCM Token Gerado: $token');
+    });
+    _fcm.configure(
+      //Aplicativo em Primeiro plano
+      // ignore: missing_return
+      onMessage: (info){
+        print('ON MESSAGE||INFO MESSAGE: $info');
+      },
+      //Aplicativo Encerrado
+      // ignore: missing_return
+      onLaunch: (info){
+        print('ON LAUNCH||INFO MESSAGE: $info');
+      },
+      //Aplicativo em Segundo plano
+      // ignore: missing_return      
+      onResume: (info){
+        print('ON RESUME||INFO MESSAGE: $info');
+      },
+    );
+    
+
+  }
+}
